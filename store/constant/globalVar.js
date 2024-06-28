f9879430c96fb7e38e3407b70fbfb5d38534d155
SudhanshuNextupgrad <sudhanshu.nextupgrad@gmail.com>
2024-06-28 18:07:37 +0530

    env added

--- store/constant/globalVar.js
+++ store/constant/globalVar.js
@@ -1,11 +1,12 @@
 export const APPCONST = {
   LOGINURL: "/admin",
+  // http://18.118.122.41/
   //API_URL: 'https://nextupgrad.com/kindness_project/',
   //API_URL: 'https://nextupgrad.com/kindness_project_api/',
   // API_URL: 'https://nextupgrad.us/laravel-old/diligent-api/',
   // MEDIA_URL: 'https://nextupgrad.us/laravel-old/diligent-api/',
-  API_URL: "http://18.118.122.41/",
-  SITE_URL: "http://18.118.122.41/",
+  API_URL: "https://kindnessbackend.azurewebsites.net/",
+  SITE_URL: "https://kindnessbackend.azurewebsites.net//",
   AccessToken: "Accesstoken",
   Dashboard: "admin/home",
   language: "english",
